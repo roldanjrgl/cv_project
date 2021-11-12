e8f4b88012da48c3b553352e0c5c37399fb8d262
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-11-12 14:20:46 -0500

    added authentication

--- radiant_mlhub/radiant-mlhub-bigearthnet_v1.py
+++ radiant_mlhub/radiant-mlhub-bigearthnet_v1.py
@@ -1,14 +1,20 @@
 import os
-os.environ['MLHUB_API_KEY'] = 'PASTE_YOUR_API_KEY_HERE'
-
 import urllib.parse
 import re
 from pathlib import Path
 import itertools as it
 from functools import partial
-
 from radiant_mlhub import client, get_session
 
+
+# ======================================================
+# Authentication
+# ======================================================
+os.environ['MLHUB_API_KEY'] = '688769cc570be590e2cf107b8418eab0aba12eb23a0a29069b06b870d6c38049'
+
+# ======================================================
+# List BigEarthNet Collections
+# ======================================================
 def ben_filter(collection):
     # NOTE: The "or" statements below ensure that if the value of collection['title'] or collection['description']
     #  are None we can still use the .lower() function without raising an exception.
