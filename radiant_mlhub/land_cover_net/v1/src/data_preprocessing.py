eb47d4e8fbb2e0dc2763ed5af19ff3279b28a58a
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-02 14:12:53 -0500

    getting png images with RGB values

---
+++ radiant_mlhub/land_cover_net/v1/src/data_preprocessing.py
@@ -0,0 +1,31 @@
+import numpy
+from fastai.vision.all import *
+import rasterio as rio
+
+def get_rgb(path_dict):
+    # reference_got_from:  https://github.com/pavlo-seimskyi/semantic-segmentation-satellite-imagery/blob/main/download_data.ipynb
+    red = rio.open(path_dict['red']).read(1) # B4
+    green = rio.open(path_dict['green']).read(1) # B3
+    blue = rio.open(path_dict['blue']).read(1) # B2
+
+    rgb = np.dstack((red, green, blue))
+
+    # normalize and convert to range 0-255
+    rgb = ((rgb - rgb.min()) / (rgb.max() - rgb.min()) * 255).astype(int)
+    return rgb
+
+def convert_images():
+    # refence_based_on: https://github.com/pavlo-seimskyi/semantic-segmentation-satellite-imagery/blob/main/download_data.ipynb
+    rgbnl = {}
+    # for loc in Path('data/ref_landcovernet_v1_labels_38PKT_25/source').ls(): 
+    for img in Path('data/ref_landcovernet_v1_labels_38PKT_29/source').ls(): 
+        print(img)
+        if re.search('.*0101_B04_10m.tif', str(img)) : rgbnl['red'] = img 
+        if re.search('.*0101_B03_10m.tif', str(img)) : rgbnl['green'] = img
+        if re.search('.*0101_B02_10m.tif', str(img)) : rgbnl['blue'] = img
+        if re.search('.*0101_B08_10m.tif', str(img)) : rgbnl['nir'] = img
+    rgb = get_rgb(rgbnl)
+    filename = 'rbd_testing'
+    plt.imsave(filename + '.png', rgb.astype('uint8'))
+
+
--- radiant_mlhub/land_cover_net/v1/src/main.py
+++ radiant_mlhub/land_cover_net/v1/src/main.py
@@ -11,6 +11,7 @@ os.environ['MLHUB_API_KEY'] = '688769cc570be590e2cf107b8418eab0aba12eb23a0a29069
 
 from mlhub_collect_data import *
 from mlhub_helper import *
+from data_preprocessing import *
 
 def main():
     collection_id = 'ref_landcovernet_v1_labels'
@@ -19,7 +20,8 @@ def main():
 
     max_items = 5
     items = get_items(collection_id, max_items=max_items)
-    download_rgb_labels_and_source(items)
+    # download_rgb_labels_and_source(items)
+    convert_images()
 
 
 if __name__ == "__main__":
