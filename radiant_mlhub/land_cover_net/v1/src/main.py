a25eb53353b02bcdfafbca63a59fd7fb30800916
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-03 13:01:35 -0500

    timing how long the downloading takes

--- radiant_mlhub/land_cover_net/v1/src/main.py
+++ radiant_mlhub/land_cover_net/v1/src/main.py
@@ -1,13 +1,6 @@
 import os
 os.environ['MLHUB_API_KEY'] = '688769cc570be590e2cf107b8418eab0aba12eb23a0a29069b06b870d6c38049'
-# import urllib.parse
-# import re
-# from pathlib import Path
-# import itertools as it
-# from functools import partial
-# from concurrent.futures import ThreadPoolExecutor
-# from tqdm.notebook import tqdm
-# from radiant_mlhub import client, get_session
+import time
 
 from mlhub_collect_data import *
 from mlhub_helper import *
@@ -20,17 +13,22 @@ def main():
     plot = False
     print_collection_info = False
     # choose number of items you want to download
-    max_items = 1
+    max_items = 5
     data_all_labels_path = './data_all_labels/ref_landcovernet_v1_labels'
     collection_id = 'ref_landcovernet_v1_labels'
 
     if print_collection_properties:
         print_collection_properties(collection_id=collection_id)
         print_land_cover_labels(collection_id)
+        
 
     if download:
-        items = get_items(collection_id,cloud_and_shadow=True, max_items=max_items)
+        start = time.time()
+        # items = get_items(collection_id,cloud_and_shadow=True, max_items=max_items)
+        items = get_items(collection_id, max_items=max_items)
         download_rgb_labels_and_source(items)
+        end = time.time()
+        print(f'Time downloading = {end - start}')
 
     if convert:
         # convert_source_to_png()
