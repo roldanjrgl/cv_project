8e2762238f9575dfec080002b801ffe9180de5c0
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-04 13:11:05 -0500

    cleaning main file

--- radiant_mlhub/su_african_crops_gana/v1/main.py
+++ radiant_mlhub/su_african_crops_gana/v1/main.py
@@ -6,36 +6,15 @@ import matplotlib.patches as mpatches
 
 os.environ['MLHUB_API_KEY'] = '688769cc570be590e2cf107b8418eab0aba12eb23a0a29069b06b870d6c38049'
 
-# def convert_label_to_png(path_to_label):
-#     label = rio.open(path_to_label).read(1)
-#     # label_png_folder = 'data_labels_png'
-#     plt.imsave('label_testing' + '.png', label)
-
 def plot_label(label):
     plt.imshow(label,cmap='tab20')
     plt.colorbar(label)
     plt.show()
     plt.legend()
     
-# def plot_label_legends(label):
-#     data = label
-
-#     plt.figure(figsize=(8,4))
-#     im = plt.imshow(data, interpolation='none')
-#     values = np.unique(data.ravel())
-
-#     # get the colors of the values, according to the 
-#     # colormap used by imshow
-#     colors = [ im.cmap(im.norm(value)) for value in values]
-#     # create a patch (proxy artist) for every color 
-#     patches = [ mpatches.Patch(color=colors[i], label="Level {l}".format(l=values[i]) ) for i in range(len(values)) ]
-#     # put those patched as legend-handles into the legend
-#     plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
-
-#     plt.grid(True)
-#     plt.show()
 
 def plot_label_legends(label):
+    # reference_based_on: https://stackoverflow.com/questions/25482876/how-to-add-legend-to-imshow-in-matplotlib
     data = label
 
     plt.figure(figsize=(8,4))
@@ -69,9 +48,15 @@ def main():
     print(type(band1))
     print(band1)
 
+    # ===================================
     # plot_label(band1)
+    # ===================================
     plot_label_legends(band1)
 
+
+    # ===================================
+    # plot_source
+    # ===================================
 
 
 
