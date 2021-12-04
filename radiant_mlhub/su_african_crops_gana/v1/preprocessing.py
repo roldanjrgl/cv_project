732ac5d6d6e5f0f0baccfd744b49ec9d03279e6b
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-04 16:33:45 -0500

    implemented convert_label_to_png() and convert_all_labels_to_png()

--- radiant_mlhub/su_african_crops_gana/v1/preprocessing.py
+++ radiant_mlhub/su_african_crops_gana/v1/preprocessing.py
@@ -1,6 +1,8 @@
+import os
 import matplotlib.pyplot as plt
 import rasterio as rio
 import numpy as np
+from fastai.vision.all import *
 
 def convert_source_to_png(data_source_path, name):
     source = rio.open(data_source_path)
@@ -15,3 +17,35 @@ def convert_source_to_png(data_source_path, name):
     rgb = ((rgb - rgb.min()) / (rgb.max() - rgb.min()) * 255).astype(int)
     # plt.imsave(source_day_dir + '/' +  source_day + '.png', rgb.astype('uint8'))
     plt.imsave(name + '.png', rgb.astype('uint8'))
+
+def convert_label_to_png(image_chip_label_path, image_chip_label_name, image_chip_name, data_png_path):
+    label = rio.open(image_chip_label_path)
+    band1 = label.read(1) 
+
+    save_to_path = data_png_path + '/' + image_chip_name
+    if os.path.isdir(save_to_path) == False:
+        os.mkdir(save_to_path)
+
+    save_to_path = save_to_path + '/labels/'
+    if os.path.isdir(save_to_path) == False:
+        os.mkdir(save_to_path)
+
+    plt.imsave(save_to_path + 'labels' + '.png', band1.astype('uint8'))
+
+
+def convert_all_labels_to_png(all_labels_path):
+    # if folder for data_png doesn't exist, create one
+    data_png_path = '../data_png'
+
+    if os.path.isdir(data_png_path) == False:
+        os.mkdir(data_png_path)
+
+    for image_chip_path in Path(all_labels_path).ls():
+        image_chip_label_name = str(image_chip_path)[-36:]
+        print(image_chip_label_name)
+        image_chip_name = image_chip_label_name[:-14] + image_chip_label_name[-7:]
+        print(image_chip_name)
+        print(image_chip_name + '/labels.tif')
+
+        image_chip_label_path = all_labels_path + '/' + image_chip_label_name + '/labels.tif'
+        convert_label_to_png(image_chip_label_path, image_chip_label_name, image_chip_name, data_png_path)
