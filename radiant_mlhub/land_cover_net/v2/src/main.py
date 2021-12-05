0d08e95d6532058c9696f3796ed6ac9320839fb2
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-05 08:43:43 -0500

    working on preprocessing for landcovernet_v2

--- radiant_mlhub/land_cover_net/v2/src/main.py
+++ radiant_mlhub/land_cover_net/v2/src/main.py
@@ -8,37 +8,11 @@ from data_preprocessing import *
 from plot_images import *
 
 def main():
-    download = False
-    convert = True
-    plot = False
-    print_collection_info = False
-    # choose number of items you want to download
-    max_items = 5
-    data_all_labels_path = './data_all_labels/ref_landcovernet_v1_labels'
-    collection_id = 'ref_landcovernet_v1_labels'
+    all_source_path = 'data_all_source'
+    all_labels_path = './data_all_labels/ref_landcovernet_v1_labels'
 
-    if print_collection_info:
-        print_collection_properties(collection_id=collection_id)
-        print_land_cover_labels(collection_id)
+    convert_all_sources_to_png(all_source_path)
+    # convert_all_labels_to_png(all_labels_path)
         
-    if download:
-        start = time.time()
-        # items = get_items(collection_id,cloud_and_shadow=True, max_items=max_items)
-        items = get_items(collection_id, max_items=max_items)
-        download_rgb_labels_and_source(items)
-        end = time.time()
-        print(f'Time downloading = {end - start}')
-
-    if convert:
-        data_path = 'data'
-        # convert_source_to_png()
-        # convert_all_labels_to_png(data_all_labels_path)
-        convert_data_to_png(data_path)
-
-    if plot:
-        label_path = 'data/ref_landcovernet_v1_labels_38PKT_29/labels/38PKT_29_2018_LC_10m.tif'
-        plot_label(label_path=label_path)
-
-
 if __name__ == "__main__":
     main()
