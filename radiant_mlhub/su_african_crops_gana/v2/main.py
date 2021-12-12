fa68a95c08e3df06b4b6f017ce05426df6aad0f8
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-12-12 16:13:26 -0500

    done processing ghana

--- radiant_mlhub/su_african_crops_gana/v2/main.py
+++ radiant_mlhub/su_african_crops_gana/v2/main.py
@@ -13,8 +13,11 @@ os.environ['MLHUB_API_KEY'] = '688769cc570be590e2cf107b8418eab0aba12eb23a0a29069
 def main():
     # explore_dataset()
 
-    all_labels_path = '../su_african_crops_ghana_labels_toy'
-    all_source_path = '../su_african_crops_ghana_source_s2_toy'
+    # all_labels_path = '../su_african_crops_ghana_labels_toy'
+    # all_source_path = '../su_african_crops_ghana_source_s2_toy'
+
+    all_labels_path = '../su_african_crops_ghana_labels'
+    all_source_path = '../su_african_crops_ghana_source_s2'
 
     convert_all_labels_to_png(all_labels_path)
     convert_all_sources_to_png(all_source_path)
